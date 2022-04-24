3ff7cb649051aba60862e62fb33721876a7eff7d
Quu <91326783+Quu23@users.noreply.github.com>
2022-04-24 21:39:01 +0900

    プレイヤーのジャンプを調整

--- pac/Player.java
+++ pac/Player.java
@@ -6,7 +6,8 @@ public class Player {
 
     int x,y;
     private static Player instance=null;
-    int jumpTime=0;
+    int jumpTime = 0;
+    int jumpSpeed = 0;
     boolean isFly=false;
 
     private Player(int x,int y){
@@ -23,11 +24,19 @@ public class Player {
 
     public void jump() {
         if(jumpTime==0){
-            jumpTime = 20;
+            jumpTime = 40;
             isFly=true;
+            jumpSpeed = 5;
         }
-        this.y += jumpTime > 10 ? -5 : 5 ;
+
+        this.y += jumpTime > 20 ? -1*jumpSpeed : jumpSpeed ;
         
+        if(jumpSpeed-20 == 7 || jumpSpeed-20 == 5 || jumpSpeed-20 == 3 || jumpSpeed-20 == 2){
+            jumpSpeed--;
+        }else if(jumpSpeed == 18 || jumpSpeed == 17 || jumpSpeed == 15 || jumpSpeed == 13){
+            jumpSpeed++;
+        }
+
         jumpTime--;
         if(jumpTime==0)isFly=false;
     }
