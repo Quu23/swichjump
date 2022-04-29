7170fb45b09c8a8263d9767d0b95ad7dc33d461b
Quu <91326783+Quu23@users.noreply.github.com>
2022-04-30 08:31:45 +0900

    床の移動の実装

--- pac/Blocks.java
+++ pac/Blocks.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Blocks {
 
     List<Block> blocks;
-    int blocksWidth=500;
+    private int blocksWidth=500;
 
     Blocks(){
         this.blocks = new ArrayList<>();
@@ -17,30 +17,30 @@ public class Blocks {
         this.blocks.add(new Block(400, 100));
     
     }
-    public void add(){
-        if(blocksWidth<500){
-            int width = new java.util.Random().nextInt(491-blocksWidth)+10;
-            int lastBlockIndex = this.blocks.size()-1;   
-            Block block = new Block(this.blocks.get(lastBlockIndex).x,width);
+    private void add(){
+        if(this.blocksWidth<500){
+            int width = new java.util.Random().nextInt(90)+50;
+            int lastBlockIndex = this.blocks.size()-1; 
+            Block block = new Block(this.blocks.get(lastBlockIndex).x+this.blocks.get(lastBlockIndex).width,width);
             this.blocksWidth += block.width;
             this.blocks.add(block);
         }
     }
-    public void move(){
+    public void move(){  
         for(int i=0;i<this.blocks.size();i++){
             if(i==0){
                 this.blocks.get(i).width--;
+                this.blocksWidth--;
             }else{
                 this.blocks.get(i).x--;
             }       
         }
-        if(this.blocks.get(0).width<1){
+        if(this.blocks.get(0).width==0){
             this.pop();
         }
-        
+        this.add();
     }
-    public void pop(){
-        this.blocksWidth -= this.blocks.get(0).width;
+    private void pop(){
         this.blocks.remove(0);
     }
 }
