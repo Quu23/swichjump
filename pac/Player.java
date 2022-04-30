28011e3ae9f7f95d4afe5b2adce4236441071b22
Quu <91326783+Quu23@users.noreply.github.com>
2022-04-30 21:27:33 +0900

    jumpメゾットを修正。より滑らかな移動が可能

--- pac/Player.java
+++ pac/Player.java
@@ -28,15 +28,17 @@ public class Player {
             isFly=true;
             jumpSpeed = 5;
         }
-
-        this.y += jumpTime > 20 ? -1*jumpSpeed : jumpSpeed ;
         
-        if(jumpSpeed-20 == 7 || jumpSpeed-20 == 5 || jumpSpeed-20 == 3 || jumpSpeed-20 == 2){
+        if(jumpTime == 27 || jumpTime == 25 || jumpTime == 23 || jumpTime == 22){
             jumpSpeed--;
-        }else if(jumpSpeed == 18 || jumpSpeed == 17 || jumpSpeed == 15 || jumpSpeed == 13){
+        }else if(jumpTime == 18 || jumpTime == 17 || jumpTime == 15 || jumpTime == 13){
             jumpSpeed++;
+            if(this.y>377&&this.y<380){
+                this.y=377;
+                jumpTime=1;
+            }
         }
-
+        this.y += jumpTime > 20 ? -1*jumpSpeed : jumpSpeed ;
         jumpTime--;
         if(jumpTime==0)isFly=false;
     }
